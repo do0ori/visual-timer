da296c45935c1d616a9d6a54ab23b1c9835ba5a5
do0ori <fuzzydo0ori@gmail.com>
2024-09-25 17:24:34 +0900

    feat: Add theme support with Tailwind CSS and react-icons
    - Integrated react-icons for icon usage across the project.
    - Added theme configuration for "classic," "purple," and "black" themes with associated colors.
    - Configured theme colors and text/bg utilities in Tailwind CSS using theme extension.
    - Introduced theme-based quotes for better UI customization in the timer component.

---
+++ src/config/timer/themes.ts
@@ -0,0 +1,69 @@
+import { themeColors } from "../themeColors";
+
+type Theme = {
+    name: string;
+    color: {
+        main: string;
+        point: string;
+        sub: string;
+    };
+    text: {
+        main: string;
+        point: string;
+        sub: string;
+    };
+    bg: {
+        main: string;
+        point: string;
+        sub: string;
+    };
+    quote: string;
+};
+
+export const themes: Record<string, Theme> = {
+    classic: {
+        name: "Classic",
+        color: themeColors.classic,
+        text: {
+            main: `text-classic-main`,
+            point: `text-classic-point`,
+            sub: `text-classic-sub`,
+        },
+        bg: {
+            main: `bg-classic-main`,
+            point: `bg-classic-point`,
+            sub: `bg-classic-sub`,
+        },
+        quote: "Great things take time.",
+    },
+    purple: {
+        name: "Purple",
+        color: themeColors.purple,
+        text: {
+            main: `text-purple-main`,
+            point: `text-purple-point`,
+            sub: `text-purple-sub`,
+        },
+        bg: {
+            main: `bg-purple-main`,
+            point: `bg-purple-point`,
+            sub: `bg-purple-sub`,
+        },
+        quote: "Becoming a better version of me.",
+    },
+    black: {
+        name: "Black",
+        color: themeColors.black,
+        text: {
+            main: `text-black-main`,
+            point: `text-black-point`,
+            sub: `text-black-sub`,
+        },
+        bg: {
+            main: `bg-black-main`,
+            point: `bg-black-point`,
+            sub: `bg-black-sub`,
+        },
+        quote: "For my better tomorrow.",
+    },
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
+const { themeColors } = require("./src/config/themeColors");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                classic: themeColors.classic,
+                purple: themeColors.purple,
+                black: themeColors.black,
+            },
+        },
     },
     plugins: [],
 };
