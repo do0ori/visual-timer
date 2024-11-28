7f74517e3d449918e5cb8bc0b9f0919d86f44e39
do0ori <fuzzydo0ori@gmail.com>
2024-11-28 14:20:27 +0900

    refactor: Move custom CSS rules to Tailwind config
    - Transferred body `overscroll-behavior` style and custom utilities (`.no-scrollbar` and `.color-picker`) to `tailwind.config.js` for better maintainability and integration with Tailwind CSS.
    - Removed redundant styles from `index.css`.
    
    This refactor centralizes styling configurations, improving scalability and consistency across the project.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,4 +3,27 @@ export const content = ['./src/**/*.{js,jsx,ts,tsx}'];
 export const theme = {
     extend: {},
 };
-export const plugins = [];
+export const plugins = [
+    function ({ addBase, addUtilities }) {
+        addBase({
+            body: {
+                'overscroll-behavior': 'none', // 모바일 당겨서 새로고침 비활성화
+            },
+        });
+
+        addUtilities({
+            '.no-scrollbar': {
+                '&::-webkit-scrollbar': {
+                    display: 'none', // Chrome, Edge, Safari
+                },
+                '-ms-overflow-style': 'none', // IE and Edge
+                'scrollbar-width': 'none', // Firefox
+            },
+            '.color-picker': {
+                '&::-webkit-color-swatch': {
+                    border: 'none', // 안쪽 테두리 제거
+                },
+            },
+        });
+    },
+];
