ea9b7801fb71847d910ca43d4ed4ed33a0ef4b48
do0ori <fuzzydo0ori@gmail.com>
2025-01-11 20:47:31 +0900

    feat: Add RepeatSwitch component for timer repeat functionality
    - Introduced a `RepeatSwitch` component for toggling between single and repeat timer modes.
    - Utilized `Switch` with options for "once" and "repeat," displaying corresponding icons.
    - Integrated theme-based styling with `currentTheme.color.point`.

---
+++ src/components/timer/controls/RepeatSwitch.tsx
@@ -0,0 +1,25 @@
+import { LuRepeat, LuRepeat1 } from 'react-icons/lu';
+import { Theme } from '../../../config/theme/themes';
+import Switch from '../../common/Switch';
+
+type RepeatSwitchProps = {
+    onClick: () => void;
+    repeat: boolean;
+    currentTheme: Theme;
+};
+
+const RepeatSwitch: React.FC<RepeatSwitchProps> = ({ onClick, repeat, currentTheme }) => {
+    return (
+        <Switch
+            options={[
+                { value: 'once', label: <LuRepeat1 /> },
+                { value: 'repeat', label: <LuRepeat /> },
+            ]}
+            value={repeat ? 'repeat' : 'once'}
+            onChange={() => onClick()}
+            backgroundColor={currentTheme.color.point}
+        />
+    );
+};
+
+export default RepeatSwitch;
