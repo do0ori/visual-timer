c7653d2b44ea53804139eedaa52eaf40ec9d260f
do0ori <fuzzydo0ori@gmail.com>
2025-01-16 01:11:18 +0900

    feat: Update feedback link in FeedbackField component
    Replaced the email feedback link with a Padlet URL for improved feedback
    collection.
    
    - Changed the `href` to point to the Padlet feedback page.
    - Added `target="_blank"` to open the link in a new tab.
    - Included `rel="noreferrer"` for security and performance enhancements.

--- src/components/timer/overlay/settings/FeedbackField.tsx
+++ src/components/timer/overlay/settings/FeedbackField.tsx
@@ -5,7 +5,11 @@ const FeedbackField: React.FC = () => {
     const IdeaIcon = <LuLightbulb size={24} className="size-full" />;
     const feedBackContent = (
         <div className="flex w-full flex-col gap-2">
-            <a href="mailto:fuzzydo0ori@gmail.com">
+            <a
+                href="https://padlet.com/fuzzydo0ori/visual-timer-feedback-ykjvyrb6887wz6zc"
+                target="_blank"
+                rel="noreferrer"
+            >
                 <div className="text-lg">Feedback</div>
             </a>
         </div>
