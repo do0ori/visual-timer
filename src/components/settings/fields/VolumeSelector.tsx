9f594d008aad38f3d7910426e5a95b2a628fdcbc
do0ori <fuzzydo0ori@gmail.com>
2025-02-27 02:05:10 +0900

    feat: Add volume toggle via icon
    - Replace IoVolumeMedium with IoVolumeHigh and IoVolumeMute.
    - Introduce handleToggleVolume to toggle volume on icon click.
    - Update volumeIcon to render mute or high icon based on volume.

--- src/components/settings/fields/VolumeSelector.tsx
+++ src/components/settings/fields/VolumeSelector.tsx
@@ -1,4 +1,4 @@
-import { IoVolumeMedium } from 'react-icons/io5';
+import { IoVolumeHigh, IoVolumeMute } from 'react-icons/io5';
 import { useSettingsStore } from '../../../store/settingsStore';
 import { useThemeStore } from '../../../store/themeStore';
 import ListItem from '../../common/ListItem';
@@ -9,7 +9,21 @@ const VolumeSelector: React.FC = () => {
 
     const pointColor = themes[globalThemeKey].color.point;
 
-    const volumeIcon = <IoVolumeMedium size={24} className="size-full" />;
+    const handleToggleVolume = () => {
+        if (volume === 0) {
+            setVolume(1);
+        } else {
+            setVolume(0);
+        }
+    };
+
+    const volumeIcon =
+        volume === 0 ? (
+            <IoVolumeMute size={24} className="size-full" onClick={handleToggleVolume} style={{ cursor: 'pointer' }} />
+        ) : (
+            <IoVolumeHigh size={24} className="size-full" onClick={handleToggleVolume} style={{ cursor: 'pointer' }} />
+        );
+
     const volumeSlider = (
         <div className="flex w-full flex-col gap-2">
             <label htmlFor="volume" className="text-lg">
