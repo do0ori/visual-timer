1d8d4490e9dc7f1012da5e16c1d3024c72c37d4d
do0ori <fuzzydo0ori@gmail.com>
2026-08-19 14:50:34 +0900

    style(settings): Simplify section layout

--- src/components/settings/sections/OthersSettings.tsx
+++ src/components/settings/sections/OthersSettings.tsx
@@ -3,13 +3,10 @@ import FeedbackField from '../fields/FeedbackField';
 import VersionField from '../fields/VersionField';
 
 const OthersSettings: React.FC = () => (
-    <div>
-        <h3 className="text-lg font-semibold">Others</h3>
-        <div className="mt-6 space-y-8">
-            <VersionField />
-            <FeedbackField />
-            <DonateField />
-        </div>
+    <div className="space-y-8">
+        <VersionField />
+        <FeedbackField />
+        <DonateField />
     </div>
 );
 
