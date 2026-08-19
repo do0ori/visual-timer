ff52b8412933ca020a240b0cefe286108fba99dc
do0ori <fuzzydo0ori@gmail.com>
2026-08-19 23:25:16 +0900

    fix(timer-list): Widen desktop timer cards

--- src/components/timers/timer-management/TimerListOverlay.tsx
+++ src/components/timers/timer-management/TimerListOverlay.tsx
@@ -343,7 +343,7 @@ const TimerListOverlay: React.FC = () => {
                   </button>
                 </div>
               ) : (
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3.5">
+                <div className="grid grid-cols-1 sm:grid-cols-2 gap-3.5">
                   {filteredTimers.map((timer: TimerData) => (
                     <div
                       key={timer.id}
